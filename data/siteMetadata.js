1fbb6621ac5f24019e2f801ecf01046a9138d3b1
Nicolas Padilla <dw.nicolaspadilla@gmail.com>
2025-04-25 20:03:19 -0300

    Cambiar título en el header

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Seguridad y algo mas',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'CobaltoSec',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
