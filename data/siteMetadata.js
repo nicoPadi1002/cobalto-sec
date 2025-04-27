df571393e7976236e81015dfd7fe079bb4cbc882
Nicolas Padilla <dw.nicolaspadilla@gmail.com>
2025-04-27 09:53:00 -0300

    Creando el primer post

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Seguridad y algo mas',
-  author: 'Tails Azimuth',
+  author: 'NP',
   headerTitle: 'CobaltoSec',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Bienvenidos',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
+  email: 'dw.nicolaspadilla@gmail.com',
   github: 'https://github.com',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
